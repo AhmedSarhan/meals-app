b33988dd0bdeed281b6c7bdb01410c03e9040d99
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2022-01-24 16:32:43 +0200

    adding drawer nav

--- routes/Drawer.navigator.js
+++ routes/Drawer.navigator.js
@@ -1,71 +1,31 @@
-import {
-	createDrawerNavigator,
-	DrawerContentScrollView,
-	DrawerItem,
-	DrawerItemList,
-} from '@react-navigation/drawer';
+import { DrawerItemList } from '@react-navigation/drawer';
 import colors from '../constants/colors';
 import Filters from '../screens/Filters';
 import AdminNavigator from './Admin.navigator';
 import BottomTabsNavigator from './TabsNavigator';
 import { SafeAreaView, Button, View } from 'react-native';
 import { useDispatch } from 'react-redux';
-import { createStackNavigator } from '@react-navigation/stack';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-
-const DrawerNav = createDrawerNavigator();
-const Stack = createStackNavigator();
-
-function CustomDrawerContent(props) {
-	const dispatch = useDispatch();
-	return (
-		<DrawerContentScrollView {...props}>
-			<DrawerItem
-				label="Home"
-				onPress={() => props.navigation.navigate('Home')}
-			/>
-			<DrawerItem
-				label="Filters"
-				onPress={() => props.navigation.navigate('Filters')}
-			/>
-			<DrawerItem
-				label="Admin"
-				onPress={() => props.navigation.navigate('Admin')}
-			/>
-			<View style={{ width: '80%', alignSelf: 'center' }}>
-				<Button
-					title="logout"
-					color="red"
-					onPress={() => dispatch(logoutAction())}
-				/>
-			</View>
-		</DrawerContentScrollView>
-	);
-}
-
-// function getHeaderTitle(route) {
-// 	return getFocusedRouteNameFromRoute(route) ?? 'Home';
-// }
-
-const StackNavigator = () => {
-	return (
-		<Stack.Navigator>
-			<Stack.Screen name="Home" component={BottomTabsNavigator} />
-			<Stack.Screen name="Filters" component={Filters} />
-			<Stack.Screen
-				options={{ headerShown: false }}
-				name="Admin"
-				component={AdminNavigator}
-			/>
-		</Stack.Navigator>
-	);
-};
 
 const DrawerNavigator = () => {
+	const dispatch = useDispatch();
 	return (
 		<DrawerNav.Navigator
-			initialRouteName="Home"
-			drawerContent={(props) => <CustomDrawerContent {...props} />}
+			drawerContent={(props) => {
+				return (
+					<View style={{ flex: 1, paddingVertical: 20 }}>
+						<SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+							<DrawerItemList {...props} />
+							<View style={{ width: '80%', alignSelf: 'center' }}>
+								<Button
+									title="logout"
+									color="red"
+									onPress={() => dispatch(logoutAction())}
+								/>
+							</View>
+						</SafeAreaView>
+					</View>
+				);
+			}}
 			screenOptions={{
 				drawerActiveTintColor: colors.accentColor,
 				drawerLabelStyle: {
@@ -75,54 +35,19 @@ const DrawerNavigator = () => {
 				},
 			}}
 		>
-			<DrawerNav.Screen name="Stack" component={StackNavigator} />
+			<DrawerNav.Screen
+				options={{ headerShown: false }}
+				name="Home"
+				component={BottomTabsNavigator}
+			/>
+			<DrawerNav.Screen name="Filters" component={Filters} />
+			<DrawerNav.Screen
+				options={{ headerShown: false }}
+				name="Admin"
+				component={AdminNavigator}
+			/>
 		</DrawerNav.Navigator>
 	);
 };
-
-// const DrawerNavigator = () => {
-//
-
-// 	return (
-// 		<DrawerNav.Navigator
-// 			drawerContent={(props) => {
-// 				return (
-// 					<View style={{ flex: 1, paddingVertical: 20 }}>
-// 						<SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
-// 							<DrawerItemList {...props} />
-// 							<View style={{ width: '80%', alignSelf: 'center' }}>
-// 								<Button
-// 									title="logout"
-// 									color="red"
-// 									onPress={() => dispatch(logoutAction())}
-// 								/>
-// 							</View>
-// 						</SafeAreaView>
-// 					</View>
-// 				);
-// 			}}
-// 			screenOptions={{
-// 				drawerActiveTintColor: colors.accentColor,
-// 				drawerLabelStyle: {
-// 					textAlign: 'center',
-// 					fontSize: 14,
-// 					fontFamily: 'open-sans-bold',
-// 				},
-// 			}}
-// 		>
-// 			<DrawerNav.Screen
-// 				options={{ headerShown: false }}
-// 				name="Home"
-// 				component={BottomTabsNavigator}
-// 			/>
-// 			<DrawerNav.Screen name="Filters" component={Filters} />
-// 			<DrawerNav.Screen
-// 				options={{ headerShown: false }}
-// 				name="Admin"
-// 				component={AdminNavigator}
-// 			/>
-// 		</DrawerNav.Navigator>
-// 	);
-// };
 
 export default DrawerNavigator;
